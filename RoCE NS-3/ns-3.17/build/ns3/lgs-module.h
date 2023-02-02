abd65c9aaf6695773947e1173e1d54ba9729e472
Tommaso Bonato <a-tbonato@microsoft.com>
2023-02-02 22:52:38 +0100

    Removed build files, folder structure improvements

---
+++ RoCE NS-3/ns-3.17/build/ns3/lgs-module.h
@@ -0,0 +1,13 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_LGS
+    
+
+// Module headers:
+#include "LogGOPSim.hpp"
+#include "logsim-helper.h"
+#include "logsim.h"
+#endif
